8d1bbca469b33917245592a4eff5c63b864b4115
Antonin <antonin.juquel@outlook.fr>
2022-04-22 00:48:00 +0200

    Added common entities components

---
+++ Assets/Scripts/Entities/Health.cs
@@ -0,0 +1,54 @@
+using Managers.Event;
+using UnityEngine;
+
+namespace Entities
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField] private int maxHealth;
+        private int _health;
+        private EntityKilledEvent _entityKilledEvent;
+
+        private void OnEnable()
+        {
+            EventHandler.Instance.AddListener<ProjectileHitEvent>(ProjectileHitHandler);
+            EventHandler.Instance.AddListener<SpaceshipCrashedEvent>(SpaceshipCrashedHandler);
+        }
+
+        private void OnDisable()
+        {
+            EventHandler.Instance.RemoveListener<ProjectileHitEvent>(ProjectileHitHandler);
+            EventHandler.Instance.RemoveListener<SpaceshipCrashedEvent>(SpaceshipCrashedHandler);
+        }
+
+        private void Awake()
+        {
+            _entityKilledEvent = new EntityKilledEvent(transform);
+            _health = maxHealth;
+        }
+
+        private void Die()
+        {
+            EventHandler.Instance.Raise(_entityKilledEvent);
+        }
+
+        private void TakeDamage(int amount)
+        {
+            _health -= amount;
+            EventHandler.Instance.Raise(new EntityDamagedEvent(transform, _health, maxHealth));
+            if (_health <= 0) Die();
+        }
+
+        private void ProjectileHitHandler(ProjectileHitEvent e)
+        {
+            if (e.Hit != transform) return;
+            TakeDamage(e.Damage);
+        }
+
+        private void SpaceshipCrashedHandler(SpaceshipCrashedEvent e)
+        {
+            if (e.Transform != transform) return;
+            TakeDamage(Mathf.CeilToInt(e.HitForce));
+        }
+    }
+}
---
+++ Assets/Scripts/Entities/Spaceship.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using Managers.Event;
+using UnityEngine;
+using Event = Managers.Event.Event;
+
+namespace Entities
+{
+    public class Spaceship : MonoBehaviour
+    {
+        [SerializeField] private float forceSpeed, torqueSpeed, crashSpeed, maxSpeed, maxFuel, burnRate;
+        private float _forceInput, _torqueInput, _fuel;
+        private bool _landed;
+        private Rigidbody2D _rb;
+        private WaitForFixedUpdate _fixedUpdate;
+        private SpaceshipLandedEvent _spaceshipLandedEvent;
+        private SpaceshipTookOffEvent _spaceshipTookOffEvent;
+        private ParticleSystem _ps;
+
+        private void Awake()
+        {
+            _ps = GetComponentInChildren<ParticleSystem>();
+            _rb = GetComponent<Rigidbody2D>();
+            _fixedUpdate = new WaitForFixedUpdate();
+            _spaceshipLandedEvent = new SpaceshipLandedEvent(transform);
+            _spaceshipTookOffEvent = new SpaceshipTookOffEvent(transform);
+
+            _fuel = maxFuel;
+        }
+
+        private IEnumerator Start()
+        {
+            while (gameObject.activeSelf)
+            {
+                if (_fuel > 0)
+                {
+                    _rb.AddForce(transform.up * forceSpeed * _forceInput);
+                    if (_forceInput != 0)
+                    {
+                        _fuel -= burnRate * Time.deltaTime;
+                        _ps.Emit(1);
+                        _rb.AddForce(Vector2.up * Mathf.Abs(Physics2D.gravity.y), ForceMode2D.Force);
+                        if (_rb.velocity.magnitude > maxSpeed)
+                        {
+                            _rb.velocity = _rb.velocity.normalized * maxSpeed;
+                        }
+                    }
+
+                    _rb.angularVelocity = -_torqueInput * torqueSpeed * (_forceInput != 0 ? .5f : 1);
+                }
+
+                var altitude = Mathf.Ceil(transform.position.y * 100);
+                var speed = Mathf.Ceil(_rb.velocity.magnitude * 100);
+
+                EventHandler.Instance.Raise(new SpaceshipMetricsEvent(transform, altitude, speed, _fuel, maxFuel));
+                _rb.drag = _forceInput;
+                yield return _fixedUpdate;
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D col)
+        {
+            var hitVelocity = col.relativeVelocity;
+            if (hitVelocity.magnitude >= crashSpeed)
+            {
+                EventHandler.Instance.Raise(new SpaceshipCrashedEvent(transform, col.relativeVelocity.magnitude));
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            _landed = false;
+            EventHandler.Instance.Raise(_spaceshipTookOffEvent);
+        }
+
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            if (!_landed && _rb.velocity == Vector2.zero)
+            {
+                _landed = true;
+                EventHandler.Instance.Raise(_spaceshipLandedEvent);
+            }
+        }
+
+        public void AddForce(float input)
+        {
+            _forceInput = input;
+        }
+
+        public void AddTorque(float input)
+        {
+            _torqueInput = input;
+        }
+    }
+}
