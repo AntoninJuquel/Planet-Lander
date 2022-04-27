a02f35183a3b798270a978a0d2b181cf52bcfdfd
Antonin <antonin.juquel@outlook.fr>
2022-04-27 13:51:01 +0200

    Added wave message and land message

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using MessagingSystem;
 using ReferenceSharing;
 using TMPro;
@@ -9,26 +10,57 @@ namespace UI
 {
     public class HUD : Screen
     {
+        [SerializeField] private Reference<bool> levelCleared;
         [SerializeField] private Reference<float> maxFuelRef, fuelRef, altitudeRef, speedRef;
         [SerializeField] private Reference<int> maxHealthRef, healthRef;
-        [SerializeField] private TextMeshProUGUI altitude, speed;
+        [SerializeField] private TextMeshProUGUI altitude, speed, land, wave;
         [SerializeField] private Image health, fuel;
-        private Transform _player;
 
         private void OnEnable()
         {
-            EventManager.Instance.AddListener<PlayerSpawnEvent>(PlayerSpawnHandler);
+            EventManager.Instance.AddListener<NewWaveEvent>(NewWaveHandler);
+            EventManager.Instance.AddListener<WaveClearedEvent>(WaveClearedHandler);
+            EventManager.Instance.AddListener<MainMenuEvent>(MainMenuHandler);
+            EventManager.Instance.AddListener<GameOverEvent>(GameOverHandler);
         }
 
         private void OnDisable()
         {
-            EventManager.Instance.RemoveListener<PlayerSpawnEvent>(PlayerSpawnHandler);
+            EventManager.Instance.RemoveListener<NewWaveEvent>(NewWaveHandler);
+            EventManager.Instance.RemoveListener<WaveClearedEvent>(WaveClearedHandler);
+            EventManager.Instance.RemoveListener<MainMenuEvent>(MainMenuHandler);
+            EventManager.Instance.RemoveListener<GameOverEvent>(GameOverHandler);
         }
 
-        private void PlayerSpawnHandler(PlayerSpawnEvent e)
+        private void NewWaveHandler(NewWaveEvent e)
         {
-            _player = e.Player;
-            fuel.fillAmount = 1;
+            if (e.Delay == 0) return;
+            var values = new string[e.Delay + 1];
+
+            for (var i = 0; i < e.Delay + 1; i++)
+            {
+                values[i] = $"PROCHAINE VAGUE DANS\n{e.Delay - i}";
+            }
+
+            StartCoroutine(wave.AnimateText(1f, values, false));
+        }
+
+        private void WaveClearedHandler(WaveClearedEvent e)
+        {
+            if (levelCleared.Value)
+            {
+                StartCoroutine(land.AnimateText(2f, new[] {"LAND", ">LAND<", ">>LAND<<", ">>>LAND<<<"}));
+            }
+        }
+
+        private void MainMenuHandler(MainMenuEvent e)
+        {
+            land.enabled = false;
+        }
+
+        private void GameOverHandler(GameOverEvent e)
+        {
+            land.enabled = false;
         }
 
         private void Update()
@@ -37,6 +69,22 @@ namespace UI
             fuel.fillAmount = fuelRef.Value / maxFuelRef.Value;
             altitude.text = $"{altitudeRef.Value} m";
             speed.text = $"{speedRef.Value} m.s<sup>-1";
+        }
+    }
+
+    public static class TextMeshProExtension
+    {
+        public static IEnumerator AnimateText(this TextMeshProUGUI text, float rate, string[] values, bool loop = true)
+        {
+            text.enabled = true;
+            var waiteForSeconds = new WaitForSeconds(1f / rate);
+            for (var i = 0; i < values.Length && text.enabled; i = loop ? (i + 1) % values.Length : i + 1)
+            {
+                text.text = values[i];
+                yield return waiteForSeconds;
+            }
+
+            text.enabled = false;
         }
     }
 }
